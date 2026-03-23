8478c1747aeb0cfc00c27d6e7ab7dc7ca5b27ac0
Raj Singh <ayanraaz3465@gmail.com>
2026-03-24 01:24:25 +0530

    fixed the issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
 
 import View from './view/View';
 import Home from './view/components/home/Home';
@@ -14,51 +13,49 @@ import Dashboard from './admin/components/dashboard/Dashboard';
 import ProductAdmin from './admin/components/product/ProductAdmin';
 import UserAdmin from './admin/components/user/UserAdmin';
 import OrderAdmin from './admin/components/order/OrderAdmin';
-// import ProductCardAdmin from './admin/components/product/product-card/ProductCardAdmin';
+import ProductDetailsAdmin from './admin/components/product-detail/ProductDetailsAdmin';
+import ProductCardAdmin from './admin/components/product/product-card/ProductCardAdmin';
 
 import NotFound from './view/components/not-found/NotFound';
 import ProductDetails from './view/components/product-detail/ProductDetails';
 import OrderDetails from './view/components/order-detail/OrderDetails';
 import LogOut from './view/components/log-out/LogOut';
 import Order from './view/components/order/Order';
-import ProductCardAdmin from './admin/components/product/product-card/ProductCardAdmin';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
+    <BrowserRouter>
+      <Routes>
 
-          {/* ADMIN ROUTES */}
-          <Route path='/admin' element={<Admin />}>
-            <Route index element={<Dashboard />} />
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='product' element={<ProductAdmin />} />
-            <Route path='user' element={<UserAdmin />} />
-            <Route path='product-details' element={<ProductDetailsAdmin />} />
-            <Route path='order' element={<OrderAdmin />} />
-            {<Route path='product-card' element={<ProductCardAdmin />} />}
-          </Route>
+        {/* ADMIN ROUTES */}
+        <Route path='/admin' element={<Admin />}>
+          <Route index element={<Dashboard />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='product' element={<ProductAdmin />} />
+          <Route path='user' element={<UserAdmin />} />
+          <Route path='product-details' element={<ProductDetailsAdmin />} />
+          <Route path='order' element={<OrderAdmin />} />
+          <Route path='product-card' element={<ProductCardAdmin />} />
+        </Route>
 
-          {/* USER ROUTES */}
-          <Route path='/' element={<View />}>
-            <Route index element={<Home />} />
-            <Route path='home' element={<Home />} />
-            <Route path='product' element={<Product />} />
-            <Route path='product-detail' element={<ProductDetails />} />
-            <Route path='cart' element={<Cart />} />
-            <Route path='order' element={<Order />} />
-            <Route path='order-detail' element={<OrderDetails />} />
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
-            <Route path='logout' element={<LogOut />} />
-            <Route path='*' element={<NotFound />} />
-          </Route>
+        {/* USER ROUTES */}
+        <Route path='/' element={<View />}>
+          <Route index element={<Home />} />
+          <Route path='home' element={<Home />} />
+          <Route path='product' element={<Product />} />
+          <Route path='product-detail' element={<ProductDetails />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='order' element={<Order />} />
+          <Route path='order-detail' element={<OrderDetails />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='logout' element={<LogOut />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
 
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
