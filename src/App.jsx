6054e3a9678a7edcb1cc60daca26d4565012a111
Raj Singh <ayanraaz3465@gmail.com>
2026-03-23 23:08:17 +0530

    Update react code

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import View from './view/View';
 import Home from './view/components/home/Home';
 import Product from './view/components/product/Product';
 import Cart from './view/components/cart/Cart';
 import Login from './view/components/login/Login';
 import Register from './view/components/register/Register';
+
 import Admin from './admin/Admin';
 import Dashboard from './admin/components/dashboard/Dashboard';
 import ProductAdmin from './admin/components/product/ProductAdmin';
 import UserAdmin from './admin/components/user/UserAdmin';
+import ProductDetailsAdmin from './admin/components/product-detail/ProductDetailsAdmin';
+import OrderAdmin from './admin/components/order/OrderAdmin';
+
 import NotFound from './view/components/not-found/NotFound';
 import ProductDetails from './view/components/product-detail/ProductDetails';
 import OrderDetails from './view/components/order-detail/OrderDetails';
 import LogOut from './view/components/log-out/LogOut';
 import Order from './view/components/order/Order';
-import ProductDetailsAdmin from './admin/components/product-detail/ProductDetailsAdmin';
-import OrderAdmin from './admin/components/order/OrderAdmin';
-import ProductCardAdmin from './product/product-card/ProductCardAdmin';
-
-
-
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* admin part */}
+
+          {/* ADMIN ROUTES */}
           <Route path='/admin' element={<Admin />}>
             <Route index element={<Dashboard />} />
+            <Route path='dashboard' element={<Dashboard />} />
             <Route path='product' element={<ProductAdmin />} />
             <Route path='user' element={<UserAdmin />} />
             <Route path='product-details' element={<ProductDetailsAdmin />} />
             <Route path='order' element={<OrderAdmin />} />
-            <Route path='dashboard' element={<Dashboard />} />
             <Route path='product-card' element={<ProductCardAdmin />} />
-
-
-
-
           </Route>
 
-          {/* view part */}
+          {/* USER ROUTES */}
           <Route path='/' element={<View />}>
             <Route index element={<Home />} />
             <Route path='home' element={<Home />} />
             <Route path='product' element={<Product />} />
             <Route path='product-detail' element={<ProductDetails />} />
-
             <Route path='cart' element={<Cart />} />
             <Route path='order' element={<Order />} />
             <Route path='order-detail' element={<OrderDetails />} />
@@ -61,16 +52,10 @@ function App() {
             <Route path='register' element={<Register />} />
             <Route path='logout' element={<LogOut />} />
             <Route path='*' element={<NotFound />} />
-
-
-
           </Route>
-
-
 
         </Routes>
       </BrowserRouter>
-
     </>
   )
 }
